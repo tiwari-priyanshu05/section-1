91c9ef22a6d6ddc15be8a2e70d816a6ae0b0a606
tiwari-priyanshu05 <priyanshutiwari139@gmail.com>
2026-06-30 09:03:10 +0530

    Landing page using various elements of Taillwind CSS

---
+++ javascript/condition.js
@@ -0,0 +1,34 @@
+num =50;
+
+if(num %2 === 0){
+    console.log("Even Number");
+//  let msg = "hello";
+//   const pi=3.14;
+//     pi=3.15;
+// //console.log(msg);
+}
+else{
+    console.log("Odd Number");
+}
+
+// const age=20;
+// if(age>=18){
+//     console.log("Eligible for voting"); 
+// }   
+//  else{
+//     console.log("Not Eligible for voting");
+// }
+ 
+a=30;
+b=40;
+c=30;
+
+if(a===b && b===c){
+    console.log("triangle is Equilateral");
+}
+else if(a===b || b===c || a===c){
+    console.log("triangle is Isosceles");
+}
+else{
+    console.log("triangle is Scalene");
+}
---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+console.log("Hello, World!");
+
+num =45;
+
+console.log(num);
+console.log( typeof(num));
+
+myname="Priyanshu";
+console.log(myname);
+console.log(typeof(myname));
+
+isHungry=false;
+console.log(isHungry);
+console.log(typeof(isHungry));
